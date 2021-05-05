a8671f196777e9dd589b0531b0865186f9cb9c95
SaharshRajput <73647463+SaharshRajput@users.noreply.github.com>
2021-05-05 15:16:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ function preload(){
 
 function setup()
 {
-    canvas=createCanvas(648,600);
-    canvas.position(110,250);
+    canvas=createCanvas(640,480);
+    canvas.position(150,150);
     video=createCapture(VIDEO);
     video.hide();
 }
@@ -14,37 +14,28 @@ function draw()
 {
     fill(255,0,0);
     stroke(255,0,0);
-    circle(50,150,70);
+    circle(50,50,80);
+ circle(590,50,80);
+circle(50,430,80);
+circle(590,430,80);
 
     fill(0,0,255);
     stroke(255,0,0);
-    rect(80, 145, 300,25);
+    rect(90, 40, 460,20);
+    rect(40, 90, 20,300);
+   rect(580, 90, 20,300);
+   rect(90, 420, 460,20);
 
-    fill(255,0,0);
-    stroke(255,0,0);
-    circle(400,155,70);
+   image(video,230,150,220,200)
+    
 
-    fill(0,0,255);
-    stroke(255,0,0);
-    rect(35, 185, 25,300);
+   
+   
 
-    fill(0,0,255);
-    stroke(255,0,0);
-    rect(390, 190, 25,300);
+   
+    
 
-
-    fill(255,0,0);
-    stroke(255,0,0);
-    circle(50,500,70);
-
-    fill(0,0,255);
-    stroke(255,0,0);
-    rect(75, 500, 300,25);
-
-    fill(255,0,0);
-    stroke(255,0,0);
-    circle(400,500,70);
-
+ 
 }
  
 function take_snapshot()
